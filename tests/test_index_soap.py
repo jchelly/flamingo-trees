55fdaac1951dc710f04c141fc638fd4906521594
John Helly <j.c.helly@durham.ac.uk>
2025-12-16 13:38:39 +0000

    Check that soap index of each trackid is correct in test

--- tests/test_index_soap.py
+++ tests/test_index_soap.py
@@ -1,11 +1,16 @@
 #!/bin/env python
 
 import h5py
+import numpy as np
 import pytest
+
 from mpi4py import MPI
 comm = MPI.COMM_WORLD
+comm_rank = comm.Get_rank()
+comm_size = comm.Get_size()
 
 from flamingo_trees.build.index_soap import make_soap_index
+
 
 @pytest.mark.mpi
 def test_index_soap(tmp_path):
@@ -25,3 +30,25 @@ def test_index_soap(tmp_path):
     output_file = tmp_path / "merger_tree.hdf5"
     with h5py.File(output_file, "w", driver="mpio", comm=comm) as f:
         make_soap_index(soap_format, first_snap, last_snap, f)
+
+    # Check the output. Do this on one rank only, for simplicity.
+    ok = True
+    if comm_rank == 0:
+        with h5py.File(output_file, "r") as tree_file:
+            for snap_nr in range(first_snap, last_snap+1):
+
+                # Read TrackId from the SOAP file
+                with h5py.File(soap_format.format(snap_nr=snap_nr), "r") as soap_file:
+                    trackid = soap_file["InputHalos/HBTplus/TrackId"][...]
+
+                # Read SOAPIndexOfTrackId from the merger tree file
+                soap_index = tree_file[f"Snapshots/{snap_nr:04d}/SOAPIndexOfTrackId"][...]
+
+                # SOAPIndexOfTrackId gives us the index where each TrackId is stored.
+                nr_halos = len(trackid)
+                ok = ok & np.all(soap_index[trackid] == np.arange(nr_halos, dtype=int))
+
+    # Ensure that if anything went wrong, all ranks abort
+    ok = comm.bcast(ok)
+    assert ok
+
