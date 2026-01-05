51bf46ad82471da171bc56eaec1a398e8a488851
John Helly <j.c.helly@durham.ac.uk>
2026-01-05 10:32:00 +0000

    Find descendants using HBT output for next snap

--- src/flamingo_trees/build/depth_first_trees.py
+++ src/flamingo_trees/build/depth_first_trees.py
@@ -18,8 +18,10 @@ import virgo.mpi.parallel_hdf5 as phdf5
 import virgo.mpi.parallel_sort as psort
 
 
-def read_halos_for_snapshot(soap_format, snap_nr, datasets):
-
+def read_soap_halos_for_snapshot(soap_format, snap_nr, datasets):
+    """
+    Read the specified properties of SOAP halos at snapshot snap_nr
+    """
     if comm_rank == 0:
         print(f"Reading snapshot: {snap_nr}")
     data = {}
@@ -37,14 +39,48 @@ def read_halos_for_snapshot(soap_format, snap_nr, datasets):
     return data
 
 
-def make_soap_trees(soap_format, first_snap, last_snap, output_file, pass_through):
+def read_hbt_descendants(hbt_dir, snap_nr, soap):
+    """
+    Read descendant info for halos at snapshot snap_nr, which requires
+    reading the HBT output for snap_nr+1.
+    """
+    # Determine number of files
+    filename = f"{hbt_dir}/{snap_nr+1:03d}/SubSnap_{snap_nr+1:03d}.0.hdf5"
+    if comm_rank == 0:
+        with h5py.File(filename, "r") as infile:
+            nr_files = int(infile["NumberOfFiles"][0])
+    else:
+        nr_files = None
+    nr_files = comm.bcast(nr_files)
 
-    # Certain datasets are always required
+    # Read the subhalo data
+    filenames = f"{hbt_dir}/{snap_nr+1:03d}/SubSnap_{snap_nr+1:03d}"+".{file_nr}.hdf5"
+    mf = phdf5.MultiFile(filenames, file_idx=np.arange(nr_files), comm=comm)
+    subhalos = mf.read("Subhalos")
+
+    # Extract quantities we need
+    trackid = np.ascontiguousarray(subhalos["TrackId"])
+    sinktrackid = np.ascontiguousarray(subhalos["SinkTrackId"])
+    snapshotindexofsink = np.ascontiguousarray(subhalos["SnapshotIndexOfSink"])
+    descendanttrackid = np.ascontiguousarray(subhalos["DescendantTrackId"])
+    del subhalos
+
+    # For each SOAP halo at snap snap_nr, find the same trackid in HBT at snap snap_nr+1
+    hbt_index = psort.parallel_match(soap["InputHalos/HBTplus/TrackId"], trackid, comm=comm)
+    assert np.all(hbt_index >= 0) # All should match
+
+    # Fetch the matching values
+    soap["NextSnapshot/SinkTrackId"] = psort.fetch_elements(sinktrackid, hbt_index, comm=comm)
+    soap["NextSnapshot/SnapshotIndexOfSink"] = psort.fetch_elements(snapshotindexofsink, hbt_index, comm=comm)
+    soap["NextSnapshot/DescendantTrackId"] = psort.fetch_elements(descendanttrackid, hbt_index, comm=comm)
+
+
+def make_soap_trees(hbt_dir, soap_format, first_snap, last_snap, output_file, pass_through):
+
+    # Certain datasets are always required from SOAP
     always_read = (
         "InputHalos/HBTplus/TrackId",
-        "InputHalos/HBTplus/DescendantTrackId",
         "InputHalos/HBTplus/LastMaxMass",
-        "InputHalos/NumberOfBoundParticles",
         )
 
     # Determine datasets we need to read
@@ -59,7 +95,7 @@ def make_soap_trees(soap_format, first_snap, last_snap, output_file, pass_throug
         nr_halos_total = 0
         for snap_nr in range(first_snap, last_snap+1):
             with h5py.File(soap_format.format(snap_nr=snap_nr), "r") as soap:
-                nr_halos_total += soap["InputHalos/NumberOfBoundParticles"].shape[0]
+                nr_halos_total += soap["InputHalos/HBTplus/TrackId"].shape[0]
         print(f"Total number of halos: {nr_halos_total}")
     else:
         nr_halos_total = None
@@ -71,14 +107,18 @@ def make_soap_trees(soap_format, first_snap, last_snap, output_file, pass_throug
     snapshot = {}
     for snap_nr in range(last_snap, first_snap-1, -1):
 
-        # Read in the halos for this snapshot
-        snapshot[snap_nr] = read_halos_for_snapshot(soap_format, snap_nr, datasets)
+        # Read in the halos for this snapshot from SOAP
+        snapshot[snap_nr] = read_soap_halos_for_snapshot(soap_format, snap_nr, datasets)
+
+        # Read in the descendant information for this snapshot from HBT
+        if snap_nr < last_snap:
+            read_hbt_descendants(hbt_dir, snap_nr, snapshot[snap_nr])
 
         if snap_nr == last_snap:
             # At the final snapshot, repartition to have a fixed number of
             # halos per rank with any extra on the last rank. This makes it
             # easy to compute what rank a halo is on given its global index.
-            local_nr_halos = len(snapshot[snap_nr]["InputHalos/NumberOfBoundParticles"])
+            local_nr_halos = len(snapshot[snap_nr]["InputHalos/HBTplus/TrackId"])
             total_nr_halos = comm.allreduce(local_nr_halos)
             nr_halos_per_rank = total_nr_halos // comm_size
             nr_sent_to_rank = np.ones(comm_size, dtype=int)*nr_halos_per_rank
@@ -92,7 +132,7 @@ def make_soap_trees(soap_format, first_snap, last_snap, output_file, pass_throug
             # snapshot, then that is the descendant. Otherwise we go with DescendantTrackId.
             # First, find the global index of any matching TrackId at the next snapshot.
             trackid1 = snapshot[snap_nr]["InputHalos/HBTplus/TrackId"]
-            descid1  = snapshot[snap_nr]["InputHalos/HBTplus/DescendantTrackId"]
+            descid1  = snapshot[snap_nr]["NextSnapshot/DescendantTrackId"]
             trackid2 = snapshot[snap_nr+1]["InputHalos/HBTplus/TrackId"]
             same_trackid_index = psort.parallel_match(trackid1, trackid2, comm=comm)
             desc_trackid_index = psort.parallel_match(descid1,  trackid2, comm=comm)
@@ -115,7 +155,6 @@ def make_soap_trees(soap_format, first_snap, last_snap, output_file, pass_throug
 
         # Assign unique identifiers to the halos
         trackid1 = snapshot[snap_nr]["InputHalos/HBTplus/TrackId"]
-        descid1  = snapshot[snap_nr]["InputHalos/HBTplus/DescendantTrackId"]
         assert np.all(trackid1 < (1 << 32))
         snapshot[snap_nr]["UniqueId"] = trackid1.astype(np.int64) + (snap_nr << 32)
 
@@ -128,6 +167,7 @@ def make_soap_trees(soap_format, first_snap, last_snap, output_file, pass_throug
             # Locate the descendant
             trackid2 = snapshot[snap_nr+1]["InputHalos/HBTplus/TrackId"]
             same_trackid_index = match(trackid1, trackid2)
+            descid1  = snapshot[snap_nr]["NextSnapshot/DescendantTrackId"]
             desc_trackid_index = match(descid1, trackid2)
             # Should either find a descendant OR have DescendantTrackId == -1
             assert np.all((same_trackid_index >= 0) | (desc_trackid_index >= 0) | (descid1 == -1))
@@ -136,6 +176,7 @@ if __name__ == "__main__":
 
     from virgo.mpi.util import MPIArgumentParser
     parser = MPIArgumentParser(comm=comm, description="Make merger trees from SOAP outputs.")
+    parser.add_argument("hbt_dir", type=str, help="Directory with HBT-HERONS output")
     parser.add_argument("soap_format", type=str, help="Format string to make SOAP filenames")
     parser.add_argument("first_snap", type=int, help="Index of the first snapshot in the simulation")
     parser.add_argument("last_snap", type=int, help="Index of the last snapshot in the simulation")
@@ -143,4 +184,4 @@ if __name__ == "__main__":
     parser.add_argument("--pass-through", type=str, default=None, help="Comma separated list of datasets to pass through")
     args = parser.parse_args()
 
-    make_soap_trees(args.soap_format, args.first_snap, args.last_snap, args.output_file, args.pass_through)
+    make_soap_trees(args.hbt_dir, args.soap_format, args.first_snap, args.last_snap, args.output_file, args.pass_through)
---
+++ tests/test_depth_first_trees.py
@@ -0,0 +1,34 @@
+#!/bin/env python
+
+import h5py
+import numpy as np
+import pytest
+from virgo.util.match import match
+
+from mpi4py import MPI
+comm = MPI.COMM_WORLD
+comm_rank = comm.Get_rank()
+comm_size = comm.Get_size()
+
+from flamingo_trees.build.depth_first_trees import make_soap_trees
+
+
+@pytest.mark.mpi
+def test_depth_first_trees(tmp_path):
+    """
+    Run the SOAP merger tree code and check the output
+    """
+    # Parameters for test run
+    hbt_dir = "./tests/data/HBT/"
+    soap_format = "./tests/data/halo_properties_{snap_nr:04d}.hdf5"
+    first_snap = 0
+    last_snap = 8
+
+    # Different MPI ranks generate different tmp paths!
+    # Here everyone will use the one from rank zero.
+    tmp_path = comm.bcast(tmp_path)
+
+    # Run the code
+    output_file = tmp_path / "merger_tree.hdf5"
+    make_soap_trees(hbt_dir, soap_format, first_snap, last_snap, output_file, None)
+
